5b8ff047e73ed7d07e5fe848f12e9fda51e89722
jpeg729 <jpeg729@gmail.com>
2013-08-06 18:08:56 +0200

    Clean-ups and small improvements to AptHistory

--- AptHistory.py
+++ AptHistory.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2011 Canonical
+# Copyright (C) 2013 jpeg729
 #
 # Author:
-#  Michael Vogt
+#  jpeg729
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -40,6 +40,10 @@ class AptHistoryEntry(object):
         self.purges = purges
 
     def get(self, op):
+        """ Parses the op pkg list and returns a pair of dictionaries
+            The first contains the non automatic changes, the second contains
+            the others. key = pkg_name, value = version_info.
+        """
         pkgs = self.__dict__[op]
         pkgs = pkgs.replace(":%s" % _arch, "")
 
@@ -51,57 +55,11 @@ class AptHistoryEntry(object):
             k, v = i.split(" (")
             v = v.strip(")")
             if "automatic" in v:
-                auto[k] = v.split(",")[0]
+                auto[k] = v.split(", automatic")[0]
             else:
                 pkg_dict[k] = v
 
         return pkg_dict, auto
-        
-    def _format_op(self, op, columns, show_auto, show_versions):
-        """ Looks through the relevant list of actions on packages and puts the
-        info in a string formatted to the screens width.
-        """
-        pkgs = self.__dict__[op]
-        if pkgs:
-            opname = "%s:" % op[:-1].capitalize()
-            
-            # Collect package names and details
-            pkgs = pkgs.replace(":%s" % _arch, "")
-            entry = None
-            entries = []
-            versions = []
-            for info in (i.strip(" ,") for i in pkgs.split(" ")):
-                if not info: continue
-                if entry is None: 
-                    entry = info  # get package name
-                elif "automatic" in info and not show_auto:
-                    entry = None
-                elif show_versions:
-                    versions.append(info.strip("()"))
-                if entry and info.endswith(")"):
-                    if versions and show_versions:
-                        entry += " (%s)" % ", ".join(versions)
-                    entries.append(entry)
-                    entry = None
-                    versions = []
-
-            # Concatenate making sure that lines don't exceed console width
-            listing = "%s %s" % (opname, ", ".join(entries))
-            out = textwrap.fill(listing, width=columns, initial_indent=' ', subsequent_indent='  ')
-            return out
-
-    def pretty_print(self, show_auto = False, show_versions = False):
-        """ Prints out the packages installed, upgraded, removed and purged in 
-        an operation. The output is adapted to the screen width with indenting
-        on the left hand side to facilitate reading the output.
-        """
-        # Get console width
-        rows, columns = os.popen('stty size', 'r').read().split()
-        columns = int(columns)
-        
-        for op in ("installs", "upgrades", "removes", "purges"):
-            out = self._format_op(op, columns, show_auto, show_versions)
-            if out: print(out)
 
     def __repr__(self):
         return "<AptHistoryEntry '%s' '%s' '%s' '%s' '%s' '%s'>" % (
@@ -110,35 +68,42 @@ class AptHistoryEntry(object):
 
 
 class AptHistoryLog(list):
-    """ list of Apt history.log entries """
-    def __init__(self, location="/var/log/apt/", before = datetime.now(), after = None):
+    """ list of AptHistoryEntry's reflecting the apt history logs """
+    def __init__(self, location="/var/log/apt/", before = datetime.now(), 
+                 after = None):
         super(AptHistoryLog, self).__init__()
         
-        if isInstance(before, str):
-            before = datetime.strptime(before, "%Y-%m-%d  %H:%M:%S")
+        if isinstance(before, str):
+            before = datetime.strptime(before.replace("  ", "_"), 
+                                       "%Y-%m-%d_%H:%M:%S")
         self.before = before
         
-        if isInstance(after, str):
-            after = datetime.strptime(after, "%Y-%m-%d  %H:%M:%S")
+        if isinstance(after, str):
+            after = datetime.strptime(after.replace("  ", "_"), 
+                                      "%Y-%m-%d  %H:%M:%S")
         if after is None:
             after = before - timedelta(30)
         self.after = after
 
         try:
             logfile = os.path.join(location, "history.log")
-            with open(logfile) as log_file:
-                self._parse_file(log_file)
+            self._parse_file(open(logfile))
         except IOError:
             pass
 
-        try:
-            logfile = os.path.join(location, "history.log.1.gz")
-            with gzip.GzipFile(logfile) as log_file:
-                self._parse_file(log_file)
-        except IOError:
-            pass
+        # Look through old log files until nothing more turns up
+        for i in range(1, 10):
+            length = len(self)
+            try:
+                logfile = os.path.join(location, "history.log.1.gz")
+                self._parse_file(gzip.GzipFile(logfile))
+            except IOError:
+                pass
+            if len(self) == length:
+                break
 
     def _parse_file(self, log_file):
+        """ Read file object in and parse the entries """
         start, end, installs, upgrades, removes, purges = "", "", "", "", "", ""
         for line in (l.strip() for l in log_file):
             if line == "" or line.startswith("#"):
@@ -148,9 +113,11 @@ class AptHistoryLog(list):
             
             #TODO parse dates into better formats
             if linetype == "Start-Date":
-                start = datetime.strptime(contents.strip(), "%Y-%m-%d  %H:%M:%S")
+                start = datetime.strptime(contents.strip(), 
+                                          "%Y-%m-%d  %H:%M:%S")
             elif linetype == "End-Date":
-                end = datetime.strptime(contents.strip(), "%Y-%m-%d  %H:%M:%S")
+                end = datetime.strptime(contents.strip(), 
+                                        "%Y-%m-%d  %H:%M:%S")
             elif linetype == "Install":
                 installs = contents.strip()
             elif linetype == "Upgrade":
@@ -173,5 +140,4 @@ if __name__ == "__main__":
     log = AptHistoryLog(location="test/data")
     for e in log:
         print("Start", e.start)
-        e.pretty_print(show_auto = True, show_versions = True)
         print(e.installs)
